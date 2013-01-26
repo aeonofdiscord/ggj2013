5e970dbfde2187b1d0b2b38a134dd5692437456b
Alex McGivern <alex@aeonofdiscord.com>
2013-01-26 11:53:14 +0000

    Tile changes; improved pulse.

--- pulse.lua
+++ pulse.lua
@@ -1,6 +1,6 @@
 local frames = {}
 
-for _,f in ipairs({'heart1', 'heart2', 'heart3', 'heart4', 'heart5', 'heart6', 'heart7', 'heart1', 'heart1', 'heart1', 'heart1', 'heart1'}) do
+for _,f in ipairs({'heart1', 'heart2', 'heart3', 'heart4', 'heart5', 'heart6', 'heart7'}) do
 	table.insert(frames, love.graphics.newImage('ui/' .. f .. '.png'))
 end
 
@@ -12,9 +12,10 @@ function PulseMonitor()
 		y = love.graphics.getHeight()-h,
 		w = w,
 		h = h,
-		frameTime = 0.1,
+		frameTime = 0.09,
 		time = 0,
 		frame = 1,
+		pulseTime = 0.01,
 	}
 	
 	function pulse:draw()
@@ -23,11 +24,16 @@ function PulseMonitor()
 	
 	function pulse:update(dtime)
 		self.time = self.time + dtime
-		if self.time >= self.frameTime then
+		if not pulsing and self.time > self.pulseTime then
+			pulsing = true
+			self.time = 0
+		end
+		if pulsing and self.time >= self.frameTime then
 			self.time = self.time - self.frameTime
 			self.frame = self.frame+1
 			if self.frame > #frames then
 				self.frame = 1
+				pulsing = false
 			end
 		end
 	end
