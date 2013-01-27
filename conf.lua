09b22448cb353a027f1846999a332fa394d2217a
CakeCatastrophe <atolm1@hotmail.com>
2013-01-27 14:41:45 +0000

    changes

--- conf.lua
+++ conf.lua
@@ -2,5 +2,5 @@ function love.conf(t)
 	t.screen.width = 1024
 	t.screen.height = 768
 	t.title = 'Procyon'
-	t.screen.fullscreen = false
+	t.screen.fullscreen = true
 end
--- text.lua
+++ text.lua
@@ -3,7 +3,7 @@ function Text(x, y, text)
 		x = x,
 		y = y,
 		text = text,
-		font = love.graphics.newFont(18)
+		font = love.graphics.newFont(14)
 	}
 	t.h = t.font:getHeight()
 	t.w = t.font:getWidth(text)
